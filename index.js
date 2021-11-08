bf32289cc38691457f7f4d302466c6a12e4c774a
shu Lin <bbqlinshu@gmail.com>
2021-11-07 22:52:39 -0500

    Completed assignment

---
+++ index.js
@@ -0,0 +1,24 @@
+function add(num1, num2){
+    return num1+num2;
+}
+function subtract(num1, num2){
+    return num1-num2;
+}
+function multiply(num1, num2){
+    return num1*num2;
+}
+function divide(num1, num2){
+    return num1/num2;
+}
+function increment(num1){
+    return num1+1;
+}
+function decrement(num1){
+    return num1-1;
+}
+function makeInt(input){
+    return parseInt(input, 10);
+}
+function preserveDecimal(input){
+    return parseFloat(input);
+}
